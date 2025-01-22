af347d64b1addbe9ffc8ffb75fb780b8eaf84064
Aiden Cary <ACARY@cub.uca.edu>
2025-01-22 17:50:12 -0600

    Created repository and made the Denomination record

---
+++ src/MakingChange/Denomination.java
@@ -0,0 +1,9 @@
+package MakingChange;
+
+public record Denomination(String name, double amt, String form, String img) {
+// String name is the name of the denomination. Ex. Ten note, Five note, Dime, Nickel, etc...
+// double amt is the amount of money the denomination represents
+// String from distinguishes if the denomination is a "bill" or a "coin"
+// String img is the string containing the image name for an image of the type of money
+
+}
