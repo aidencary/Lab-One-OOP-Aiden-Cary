03252088933d46c55dce70cc49b3d1aada0e6947
Aiden Cary <ACARY@cub.uca.edu>
2025-01-22 20:16:20 -0600

    Created the Denomination record and implemented Purse class

--- src/MakingChange/Denomination.java
+++ src/MakingChange/Denomination.java
@@ -5,5 +5,4 @@ public record Denomination(String name, double amt, String form, String img) {
 // double amt is the amount of money the denomination represents
 // String from distinguishes if the denomination is a "bill" or a "coin"
 // String img is the string containing the image name for an image of the type of money
-
 }
---
+++ src/MakingChange/Purse.java
@@ -0,0 +1,48 @@
+package MakingChange;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Purse {
+    // Uses a hash map to keep track of the number of each denomination in the purse.
+    private final Map<Denomination, Integer> cash = new HashMap<>();
+
+    // Add a specific number of a denomination to the purse
+    public void add(Denomination type, int num) {
+        // Update the count or set it if it is not present
+        cash.put(type, cash.getOrDefault(type, 0) + num);
+    }
+
+    // Remove a specific number of a denomination and return the total value removed
+    public double remove(Denomination type, int num) {
+        int currentCount = cash.getOrDefault(type, 0); // Gets the current count of the denomination
+        int toRemove = Math.min(num, currentCount); // Makes sure that we don't remove more than what there is available
+        cash.put(type, currentCount - toRemove); // Updates the count in the map
+        return toRemove * type.amt(); // Return the total value that is removed
+    }
+
+    // Calculate the total value of the purse
+    public double getValue() {
+        double total = 0.0;
+        for (Map.Entry<Denomination, Integer> entry : cash.entrySet()) {
+            Denomination denom = entry.getKey(); // Gets the key for the Denomination
+            int amount = entry.getValue(); // Gets the value
+
+            // Add to the total
+            total += denom.amt() * amount; // Reps the denom's worth per piece * the number of pieces
+        }
+        return total;
+    }
+
+    // Provides a string representation of the purse contents
+    public String toString() {
+        StringBuilder sb = new StringBuilder("Purse contents:\n"); // May need to change this
+        for (Map.Entry<Denomination, Integer> entry : cash.entrySet()) {
+            sb.append(entry.getKey().name()) // Appends the name of the denomination
+                    .append(" x ") // Appends the count prefix
+                    .append(entry.getValue()) // Appends the count
+                    .append("\n");
+        }
+        return sb.toString();
+    }
+}
